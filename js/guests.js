b4ca68f9a50b819da58aca26af5b98965c233211
Peter Maroti <talk@marotipeter.com>
2018-03-11 16:07:22 +0100

    add - backend api polling for current user data

--- js/guests.js
+++ js/guests.js
@@ -10,140 +10,63 @@ function GuestList (left, right) {
       rowClass: 'flight-board-arrivals__header'
     }
   ];
-  this.list = {
-    left: [
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Késik',
-        statusClass: 'text-red',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Késik',
-        statusClass: 'text-red',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
+  this.list = mockData;
+  this.updateInterval = 60000;
+  this.apiUrl = 'https://github.com/JarJarMP/WeddingFlightMonitor';
+
+  this.init();
+}
+
+GuestList.prototype.init = function () {
+  const self = this;
+
+  self.updateData();
+  window.setInterval(self.updateData.bind(self), self.updateInterval);
+}
+
+GuestList.prototype.updateData = function () {
+  const self = this;
+
+  self
+    .makeRequest('GET', self.apiUrl)
+    .then(function (data) {
+      console.log(data);
+      /*self.list = data;
+      self.render();*/
+    })
+    .catch(function (err) {
+      console.error('No user data, backend seems to be down!', err.statusText);
+      self.list = mockData;
+      self.render();
+    });
+}
+
+GuestList.prototype.makeRequest = function (method, url) {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
       }
-    ],
-    right: [
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Késik',
-        statusClass: 'text-red',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Késik',
-        statusClass: 'text-red',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi az irdatlan hosszú nevű pék',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged és csodálatos környéke',
-        status: 'Úton',
-        statusClass: '',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
-      },
-      {
-        name: 'Józsi',
-        nameClass: 'text-yellow',
-        placement: 'Szeged',
-        status: 'Megérkezett',
-        statusClass: 'text-green',
-        rowClass: 'flight-board-arrivals__guest'
-      }
-    ]
-  };
+    };
+
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+
+    xhr.send();
+  });
 }
 
 GuestList.prototype.render = function () {
@@ -169,3 +92,137 @@ GuestList.prototype.getOneRow = function (data) {
 GuestList.prototype.setHTML = function (classSelector, html) {
   document.getElementsByClassName(classSelector)[0].innerHTML = html;
 }
+
+const mockData = {
+  left: [
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Késik',
+      statusClass: 'text-red',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Késik',
+      statusClass: 'text-red',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    }
+  ],
+  right: [
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Késik',
+      statusClass: 'text-red',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Késik',
+      statusClass: 'text-red',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi az irdatlan hosszú nevű pék',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged és csodálatos környéke',
+      status: 'Úton',
+      statusClass: '',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    },
+    {
+      name: 'Józsi',
+      nameClass: 'text-yellow',
+      placement: 'Szeged',
+      status: 'Megérkezett',
+      statusClass: 'text-green',
+      rowClass: 'flight-board-arrivals__guest'
+    }
+  ]};
